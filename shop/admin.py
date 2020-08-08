f9d674882e3ba71aea498c902b913d676bc6215f
RiswanBasha <riswanbasha26@gmail.com>
2020-08-08 18:49:52 +0530

    updated commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Products,Order
 # Register your models here.
-admin.site.register(Products)
+admin.site.site_header="A2Z Cart Admin"
+admin.site.site_title= "A2Z Cart"
+admin.site.index_title= "Manage A2Z Cart Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+
+
+    list_display = ('title','price','discount_price','category','description')
+    search_fields= ('title',)
+    actions= ('change_category_to_default',)
+    list_editable=('price','category',)
+
+admin.site.register(Products,ProductAdmin)
 admin.site.register(Order)
